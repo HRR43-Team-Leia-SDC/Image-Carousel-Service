608f426bb791219b25bdd7fb9420045fdcfcc0d8
Dan Moy <danmoy69@gmail.com>
2020-02-24 15:50:29 -0800

    updated k6 script and webpack config

--- database/k6Script.js
+++ database/k6Script.js
@@ -2,7 +2,8 @@ import http from 'k6/http';
 import { sleep } from 'k6';
 
 export default function() {
-  http.get(`http://localhost:5000?4913406`);
-  http.get(`http://localhost:5000/carousel/4913406`);
+  const randomNum = Math.floor(Math.random() * 10000000);
+  http.get(`http://localhost:5000/?${randomNum}`);
+  http.get(`http://localhost:5000/carousel/${randomNum}`);
   sleep(1);
 }
